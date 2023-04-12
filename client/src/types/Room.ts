621db8dbdc520f668657e2bd26314bf31cdf9d56
Jakov Tomasić <jakov.tomasic@invariant.hr>
2023-04-12 19:20:08 +0200

    added room type

--- client/src/types/Room.ts
+++ client/src/types/Room.ts
@@ -3,10 +3,12 @@ export class Room {
     id: number;
     endNodeId: number;
     names: string[];
+    type: string;
 
-    constructor(id: number, endNodeId: number, names: string[]) {
+    constructor(id: number, endNodeId: number, names: string[], type: string) {
         this.id = id;
         this.endNodeId = endNodeId;
         this.names = names;
+        this.type = type;
     }
 }

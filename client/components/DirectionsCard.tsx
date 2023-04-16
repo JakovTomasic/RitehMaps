46ffb75136ebba256ebc41ea23b254e4f2ae75d8
Marko Kozlov <markokozlov@gmail.com>
2023-04-16 17:45:14 +0200

    Added zoomable svg image of the faculty layout. Implemented node adding and connecting functionality.

--- client/components/DirectionsCard.tsx
+++ client/components/DirectionsCard.tsx
@@ -7,23 +7,25 @@ export default function DirectionsCard( {text} ){
 
     return(
         <>
-            <div className="py-7 px-5 mx-4 rounded-2xl bg-blue-600 rounded-bl-none">
-                <div className="grid grid-cols-6 items-center">
-                    <div className="col-span-2 pl-4 max-[300px]:w-14">
-                        <Image src={up_right} alt="arrow" />
-                    </div>
-                    <div className="col-span-4">
-                        <p className="font-medium text-md min-[300px]:text-xl text-white">{text.current}</p>
+            <div className="mb-8">
+                <div className="py-7 px-5 mx-4 rounded-2xl bg-blue-600 rounded-bl-none">
+                    <div className="grid grid-cols-6 items-center">
+                        <div className="col-span-2 pl-4 max-[300px]:w-14">
+                            <Image src={up_right} alt="arrow" />
+                        </div>
+                        <div className="col-span-4">
+                            <p className="font-medium text-md min-[300px]:text-xl text-white">{text.current}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="pb-3 pt-3 px-5 ml-4 rounded-2xl bg-blue-800 shadow-md rounded-t-none w-2/3">
-                <div className="grid grid-cols-6 items-center">
-                    <div className="col-span-2 max-[300px]:w-8">
-                        <Image src={up} alt="arrow" />
-                    </div>
-                    <div className="col-span-4">
-                        <p className="font-medium text-sm min-[300px]:text-lg text-white">{text.next}</p>
+                <div className="pb-3 pt-3 px-5 ml-4 rounded-2xl bg-blue-800 shadow-md rounded-t-none w-2/3">
+                    <div className="grid grid-cols-6 items-center">
+                        <div className="col-span-2 max-[300px]:w-8">
+                            <Image src={up} alt="arrow" />
+                        </div>
+                        <div className="col-span-4">
+                            <p className="font-medium text-sm min-[300px]:text-lg text-white">{text.next}</p>
+                        </div>
                     </div>
                 </div>
             </div>
--- client/src/pages/navigation.tsx
+++ client/src/pages/navigation.tsx
@@ -3,21 +3,26 @@ import Button from "../../components/Button";
 import Header from "../../components/Header";
 import DirectionsCard from "../../components/DirectionsCard";
 import prizemlje_gl from "../../images/prizemlje_gl.png";
+import DrawMap from "../../components/DrawMap";
 
 export default function Navigation(){
+
 
     return(
         <>
             <div className="relative h-screen w-screen">
                 <Header text={{value: 'Navigation'}} />
                 <DirectionsCard text={{current: 'Skrenite desno', next: 'Idite ravno'}} />
-                <Image className="mb-9 mt-5" src={prizemlje_gl} alt="prizemlje" />
-                <div className="text-center justify-center flex mx-auto mb-4 inset-x-0 absolute bottom-20">
+                <div>
+                    <DrawMap />
+                </div>
+                <div className="text-center justify-center flex mx-auto inset-x-0 absolute bottom-0 my-12">
                     <Button text={{value: 'Back'}} />
                     <Button text={{value: 'Update'}} />
                     <Button text={{value: 'Next'}} />
                 </div>
             </div>
+            
         </>
     );
 }

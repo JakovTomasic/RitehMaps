dd2eb6f4631fc1403deef4b0341737e8e8ba3a58
Marko Kozlov <markokozlov@gmail.com>
2023-04-16 17:45:14 +0200

    Added zoomable svg image of the faculty layout. Implemented node adding and connecting functionality.

--- client/src/pages/navigation.tsx
+++ client/src/pages/navigation.tsx
@@ -3,8 +3,10 @@ import Button from "../../components/Button";
 import Header from "../../components/Header";
 import DirectionsCard from "../../components/DirectionsCard";
 import groundFloor_main from "../../images/groundFloor_main.png";
+import DrawMap from "../../components/DrawMap";
 
 export default function Navigation(){
+
 
     return(
         <>
@@ -12,13 +14,16 @@ export default function Navigation(){
                 <Header text='Navigation' backPath='/' />
                 <DirectionsCard currentText='Turn right' nextText='Go straight' 
                     currentDirection='up-right' nextDirection='up' />
-                <Image className="mb-9 mt-5" src={groundFloor_main} alt="ground floor main building" />
-                <div className="text-center justify-center flex mx-auto mb-4 inset-x-0 absolute bottom-20">
+                <div>
+                    <DrawMap />
+                </div>             
+                <div className="text-center justify-center flex mx-auto mb-4 inset-x-0 absolute bottom-0 my-12">
                     <Button text='Back' />
                     <Button text='Update' />
                     <Button text='Next' />
                 </div>
             </div>
+            
         </>
     );
 }

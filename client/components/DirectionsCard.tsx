cfb8807932e58a9c96b87e610b79cb24ae0a3723
Marko132001 <markokozlov@gmail.com>
2023-04-17 14:53:46 +0200

    Rebasing path_drawing branch on main

--- client/components/DirectionsCard.tsx
+++ client/components/DirectionsCard.tsx
@@ -5,23 +5,25 @@ export default function DirectionsCard( {currentText, nextText, currentDirection
 
     return(
         <>
-            <div className="py-7 px-5 mx-4 rounded-2xl bg-cyan-500 rounded-bl-none">
-                <div className="grid grid-cols-6 items-center">
-                    <div className="col-span-2 pl-4 max-[300px]:w-14">
-                        <Image src={`/images/${currentDirection}.png`} width={48} height={48} alt="arrow" />
-                    </div>
-                    <div className="col-span-4">
-                        <p className="font-medium text-md min-[300px]:text-xl text-white">{currentText}</p>
+            <div className="mb-8">
+                <div className="py-7 px-5 mx-4 rounded-2xl bg-cyan-500 rounded-bl-none">
+                    <div className="grid grid-cols-6 items-center">
+                        <div className="col-span-2 pl-4 max-[300px]:w-14">
+                            <Image src={`/images/${currentDirection}.png`} width={48} height={48} alt="arrow" />
+                        </div>
+                        <div className="col-span-4">
+                            <p className="font-medium text-md min-[300px]:text-xl text-white">{currentText}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="pb-3 pt-3 px-5 ml-4 rounded-2xl bg-cyan-700 shadow-md rounded-t-none w-2/3">
-                <div className="grid grid-cols-6 items-center">
-                    <div className="col-span-2 max-[300px]:w-8">
-                        <Image src={`/images/${nextDirection}.png`} width={48} height={48} alt="arrow" />
-                    </div>
-                    <div className="col-span-4">
-                        <p className="font-medium text-sm min-[300px]:text-lg text-white">{nextText}</p>
+                <div className="pb-3 pt-3 px-5 ml-4 rounded-2xl bg-cyan-700 shadow-md rounded-t-none w-2/3">
+                    <div className="grid grid-cols-6 items-center">
+                        <div className="col-span-2 max-[300px]:w-8">
+                            <Image src={`/images/${nextDirection}.png`} width={48} height={48} alt="arrow" />
+                        </div>
+                        <div className="col-span-4">
+                            <p className="font-medium text-sm min-[300px]:text-lg text-white">{nextText}</p>
+                        </div>
                     </div>
                 </div>
             </div>

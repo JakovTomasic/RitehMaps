e4cac3955800023d7b546c346bafdb5a0a87810b
Marko Kozlov <markokozlov@gmail.com>
2023-04-17 09:28:23 +0200

    Made changes based on code review. Changed Button and DirectionsCard color.

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,15 +1,20 @@
-import arrow from "../images/arrow.png";
+import back_arrow from "../images/back_arrow.png";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Header( {text} ){
+export default function Header( {text, homePath}: Prop ){
 
     return(
         <>
             <div className="flex flex-row align-middle my-6 mx-3 font-mono">
-                <Link href="/"><Image src={arrow} alt="arrow" /></Link>
-                <h1 className="text-xl ml-2">{text.value}</h1>
+                <Link href={homePath}><Image src={back_arrow} alt="arrow" /></Link>
+                <h1 className="text-xl ml-2">{text}</h1>
             </div>
         </>
     );
+}
+
+type Prop = {
+    text: string;
+    homePath: string;
 }

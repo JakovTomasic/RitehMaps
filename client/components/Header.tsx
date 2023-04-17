12a12dd4f89a9eacaa60f55d5c119740b827ab3e
Marko Kozlov <markokozlov@gmail.com>
2023-04-17 13:39:58 +0200

    Translating certain strings and files to english.

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -2,12 +2,12 @@ import back_arrow from "../images/back_arrow.png";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Header( {text, homePath}: Prop ){
+export default function Header( {text, backPath}: Prop ){
 
     return(
         <>
             <div className="flex flex-row align-middle my-6 mx-3 font-mono">
-                <Link href={homePath}><Image src={back_arrow} alt="arrow" /></Link>
+                <Link href={backPath}><Image src={back_arrow} alt="arrow" /></Link>
                 <h1 className="text-xl ml-2">{text}</h1>
             </div>
         </>
@@ -16,5 +16,5 @@ export default function Header( {text, homePath}: Prop ){
 
 type Prop = {
     text: string;
-    homePath: string;
+    backPath: string;
 }
--- client/src/pages/navigation.tsx
+++ client/src/pages/navigation.tsx
@@ -2,17 +2,17 @@ import Image from "next/image";
 import Button from "../../components/Button";
 import Header from "../../components/Header";
 import DirectionsCard from "../../components/DirectionsCard";
-import prizemlje_gl from "../../images/prizemlje_gl.png";
+import groundFloor_main from "../../images/groundFloor_main.png";
 
 export default function Navigation(){
 
     return(
         <>
             <div className="relative h-screen w-screen">
-                <Header text='Navigation' homePath='/' />
-                <DirectionsCard currentText='Skrenite desno' nextText='Idite ravno' 
+                <Header text='Navigation' backPath='/' />
+                <DirectionsCard currentText='Turn right' nextText='Go straight' 
                     currentDirection='up-right' nextDirection='up' />
-                <Image className="mb-9 mt-5" src={prizemlje_gl} alt="prizemlje" />
+                <Image className="mb-9 mt-5" src={groundFloor_main} alt="ground floor main building" />
                 <div className="text-center justify-center flex mx-auto mb-4 inset-x-0 absolute bottom-20">
                     <Button text='Back' />
                     <Button text='Update' />

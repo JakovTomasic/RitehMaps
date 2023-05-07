01af2d396c3dd53d28d108532373fb162b793e9b
Nika <78023750+NikaRodin@users.noreply.github.com>
2023-05-07 23:21:32 +0200

    recreatePath function refactored

--- client/src/logic/impl/pathfinding/recreatePath.ts
+++ client/src/logic/impl/pathfinding/recreatePath.ts
@@ -2,35 +2,32 @@ import { MapNode } from "../../../types/graph/MapNode";
 import { Graph } from "../../interfaces/Graph";
 
 /**
- * Recreates the path in the correct order from the given `parents` map.
+ * Recreates the path in the correct order (from start node to destination node) using the given `parents` map.
  * 
- * @param lastNodeId the ID of the destination node. This node will be the last element in the returned list.
- * @param parents must be of type `Map<number, number>`. It should contain child nodes mapped to their corresponding parent nodes. The first element (key) is the child node ID, and the second element (value) is the parent node ID. 
- * @param graph must be of type `Graph`. The actual nodes (`MapNode`) are retrieved from this graph using their IDs.
- * @returns a list of `MapNode` elements. This represents the path in the correct order (from start node to destination node). The destination node should be provided through `lastNodeId` parameter.
+ * @param parents The first element (key) is the child node ID, and the second element (value) is the parent node ID. 
  */
 
-export function recreatePath(lastNodeId: number, parents: Map<number, number>, graph: Graph): MapNode[] {
+export function recreatePath(destinationNodeId: number, parents: Map<number, number>, graph: Graph): MapNode[] {
 
     let path: MapNode[] = [];
     let parentId: number;
     let parentNode: MapNode;
-    let lastNode: MapNode = graph.getNode(lastNodeId);
+    let destinationNode: MapNode = graph.getNode(destinationNodeId);
 
-    if(lastNode === undefined)
-        throw new Error("Node with id = " + lastNodeId + " does not exist in the provided graph.");
+    if(destinationNode === undefined)
+        throw new Error("Node with id = " + destinationNodeId + " does not exist in the provided graph.");
 
-    path.push(lastNode);
+    path.push(destinationNode);
 
-    while (parents.has(lastNodeId)) {
-        parentId = parents.get(lastNodeId);
+    while (parents.has(destinationNodeId)) {
+        parentId = parents.get(destinationNodeId);
         parentNode = graph.getNode(parentId);
 
         if(parentNode === undefined)
             throw new Error("Node with id = " + parentId + " does not exist in the provided graph.");
 
         path.push(parentNode);
-        lastNodeId = parentId;
+        destinationNodeId = parentId;
     }
     return path.reverse();
 }
--- client/tests/recreatePath.test.ts
+++ client/tests/recreatePath.test.ts
@@ -7,7 +7,7 @@ describe('testing recreatePath()', () => {
   var endNodesGraph = new EndNodesGraphMock;
 
   test('should return correct recreated path', () => {
-    var lastNodeId = 5;
+    var destinationNodeId = 5;
     var parents = new Map<number, number>([[2, 1], [3, 2], [4, 3], [5, 4]]);
     var expectedPath = [new EndNode(1, 1, 0, 0), 
                         new EndNode(2, 1, 0, 0),
@@ -15,23 +15,23 @@ describe('testing recreatePath()', () => {
                         new EndNode(4, 1, 0, 0), 
                         new EndNode(5, 1, 0, 0)];
 
-    var path = recreatePath(lastNodeId, parents, endNodesGraph);
+    var path = recreatePath(destinationNodeId, parents, endNodesGraph);
     expect(path).toEqual(expectedPath);
   });
 
-  test('should return error due to incorrect lastNodeId', () => {
-    var lastNodeId = 10;
+  test('should return error due to incorrect destinationNodeId', () => {
+    var destinationNodeId = 10;
     var parents = new Map<number, number>([[2, 1], [3, 2], [4, 3], [10, 4]]);
     var errorMessage = "Node with id = 10 does not exist in the provided graph.";
 
-    expect(() =>  recreatePath(lastNodeId, parents, endNodesGraph)).toThrowError(errorMessage);
+    expect(() =>  recreatePath(destinationNodeId, parents, endNodesGraph)).toThrowError(errorMessage);
   });
 
   test('should return error due to incorrect parentId', () => {
-    var lastNodeId = 5;
+    var destinationNodeId = 5;
     var parents = new Map<number, number>([[2, 1], [3, 2], [4, -1], [5, 4]]);
     var errorMessage = "Node with id = -1 does not exist in the provided graph.";
 
-    expect(() =>  recreatePath(lastNodeId, parents, endNodesGraph)).toThrowError(errorMessage);
+    expect(() =>  recreatePath(destinationNodeId, parents, endNodesGraph)).toThrowError(errorMessage);
   });
 });

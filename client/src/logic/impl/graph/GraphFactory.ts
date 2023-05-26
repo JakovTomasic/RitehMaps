e175d484698c98e1f84bd5e654d0527ac33af85e
Jakov Tomasić <jakov.tomasic@invariant.hr>
2023-05-26 17:13:27 +0200

    GraphFactory.ts refactor

--- client/src/logic/impl/graph/GraphFactory.ts
+++ client/src/logic/impl/graph/GraphFactory.ts
@@ -4,6 +4,7 @@ import { Hallway } from "../../../data/Hallways";
 import { Node } from "../../../data/Nodes";
 import { MapNode } from "../../../types/graph/MapNode";
 import { Dot, Line, calculateLinesIntersection, calculateProjection } from "../../../utils/Geometry";
+import { Edge } from "../../../data/Edges";
 
 export type GraphNode = {
     node: MapNode;
@@ -11,40 +12,20 @@ export type GraphNode = {
 }
 
 export function createGraph(graphData: AllGraphData): Map<string, GraphNode> {
-    let graph = new Map<string, GraphNode>();
-    let hallways = new Map<string, Hallway>();
-    let allHallwayProjections = new Map<string, GraphNode[]>();
+    const graph = new Map<string, GraphNode>();
 
     addAllNodesWithoutConnections(graph, graphData.nodes);
+    joinAllAddedGraphNodes(graph, graphData.edges);
 
-    graphData.hallways.forEach(hallway => {
-        hallways.set(hallway.id, hallway);
-    });
-
-    graphData.edges.forEach(edge => {
-        let node1: GraphNode | undefined = graph.get(edge.nodeId1);
-        let node2: GraphNode | undefined = graph.get(edge.nodeId2);
-        
-        if (node1 !== undefined && node2 !== undefined) {
-            node1.neighbours.push(node2.node);
-            node2.neighbours.push(node1.node);
-        } else if (hallways.has(edge.nodeId1) && hallways.has(edge.nodeId2)) {
-            joinTwoHallways(allHallwayProjections, hallways.get(edge.nodeId1), hallways.get(edge.nodeId2));
-        } else if (hallways.has(edge.nodeId1) && node2 !== undefined) {
-            addHallwayProjectionOfNode(allHallwayProjections, hallways.get(edge.nodeId1), node2);
-        } else if (hallways.has(edge.nodeId2) && node1 !== undefined) {
-            addHallwayProjectionOfNode(allHallwayProjections, hallways.get(edge.nodeId2), node1);
-        }
-    });
-
-    joinAllHallwayProjections(allHallwayProjections, graph);
+    const hallwayNodeProjections = createNodeProjectionsOntoHallways(graph, graphData.hallways, graphData.edges);
+    joinAllHallwayProjections(hallwayNodeProjections, graph);
 
     return graph;
 }
 
 function addAllNodesWithoutConnections(graph: Map<string, GraphNode>, nodes: Node[]) {
     nodes.forEach(node => {
-        let graphNode: GraphNode = {
+        const graphNode: GraphNode = {
             node: {
                 id: node.nodeId,
                 submapId: node.submapId,
@@ -57,43 +38,59 @@ function addAllNodesWithoutConnections(graph: Map<string, GraphNode>, nodes: Nod
     });
 }
 
-function addHallwayProjectionOfNode(allHallwayProjections: Map<string, GraphNode[]>, hallway: Hallway, node: GraphNode) {
-    
-    assert(node.node.submapId === hallway.submapId);
+function joinAllAddedGraphNodes(graph: Map<string, GraphNode>, edges: Edge[]) {
+    edges.forEach(edge => {
+        const node1: GraphNode | undefined = graph.get(edge.nodeId1);
+        const node2: GraphNode | undefined = graph.get(edge.nodeId2);
+        
+        if (node1 !== undefined && node2 !== undefined) {
+            node1.neighbours.push(node2.node);
+            node2.neighbours.push(node1.node);
+        }
+    });
+}
 
-    let dot: Dot = {x: node.node.xCoordinate, y: node.node.yCoordinate};
-    let line: Line = hallwayToLine(hallway);
+function createNodeProjectionsOntoHallways(
+    graph: Map<string, GraphNode>,
+    hallways: Hallway[],
+    edges: Edge[]
+): Map<string, GraphNode[]> {
+    const hallwaysMap = new Map<string, Hallway>();
+    const allHallwayProjections = new Map<string, GraphNode[]>();
 
-    let projection = calculateProjection(dot, line);
+    hallways.forEach(hallway => {
+        hallwaysMap.set(hallway.id, hallway);
+    });
 
-    let id: string = `p:${hallway.id}->${node.node.id}`;
+    edges.forEach(edge => {
+        const node1: GraphNode | undefined = graph.get(edge.nodeId1);
+        const node2: GraphNode | undefined = graph.get(edge.nodeId2);
+        
+        if (hallwaysMap.has(edge.nodeId1) && hallwaysMap.has(edge.nodeId2)) {
+            joinTwoHallways(allHallwayProjections, hallwaysMap.get(edge.nodeId1), hallwaysMap.get(edge.nodeId2));
+        } else if (hallwaysMap.has(edge.nodeId1) && node2 !== undefined) {
+            projectNodeOntoHallway(allHallwayProjections, hallwaysMap.get(edge.nodeId1), node2);
+        } else if (hallwaysMap.has(edge.nodeId2) && node1 !== undefined) {
+            projectNodeOntoHallway(allHallwayProjections, hallwaysMap.get(edge.nodeId2), node1);
+        }
+    });
 
-    let graphNode: GraphNode = {
-        node: {
-            id: id,
-            submapId: node.node.submapId,
-            xCoordinate: projection.x,
-            yCoordinate: projection.y,
-        },
-        neighbours: [node.node]
-    };
-    node.neighbours.push(graphNode.node);
-    addAllHallwayProjection(allHallwayProjections, hallway.id, graphNode);
+    return allHallwayProjections;
 }
 
 function joinTwoHallways(allHallwayProjections: Map<string, GraphNode[]>, hallway1: Hallway, hallway2: Hallway) {
 
     assert(hallway1.submapId === hallway2.submapId);
 
-    let line1 = hallwayToLine(hallway1);
-    let line2 = hallwayToLine(hallway2);
-    let intersection = calculateLinesIntersection(line1, line2);
+    const line1 = hallwayToLine(hallway1);
+    const line2 = hallwayToLine(hallway2);
+    const intersection = calculateLinesIntersection(line1, line2);
 
-    let id: string = `p:${hallway1.id}->${hallway2.id}`;
+    const projectionId: string = `p:${hallway1.id}->${hallway2.id}`;
 
-    let graphNode: GraphNode = {
+    const graphNode: GraphNode = {
         node: {
-            id: id,
+            id: projectionId,
             submapId: hallway1.submapId,
             xCoordinate: intersection.x,
             yCoordinate: intersection.y,
@@ -101,28 +98,53 @@ function joinTwoHallways(allHallwayProjections: Map<string, GraphNode[]>, hallwa
         neighbours: []
     };
 
-    addAllHallwayProjection(allHallwayProjections, hallway1.id, graphNode);
-    addAllHallwayProjection(allHallwayProjections, hallway2.id, graphNode);
+    addHallwayProjection(allHallwayProjections, hallway1.id, graphNode);
+    addHallwayProjection(allHallwayProjections, hallway2.id, graphNode);
 }
 
-function addAllHallwayProjection(allHallwayProjections: Map<string, GraphNode[]>, hallwayId: string, graphNode: GraphNode) {
-    if (!allHallwayProjections.has(hallwayId)) {
-        allHallwayProjections.set(hallwayId, [graphNode]);
+function projectNodeOntoHallway(allHallwayProjections: Map<string, GraphNode[]>, hallway: Hallway, node: GraphNode) {
+    
+    assert(node.node.submapId === hallway.submapId);
+
+    const dot: Dot = {x: node.node.xCoordinate, y: node.node.yCoordinate};
+    const line: Line = hallwayToLine(hallway);
+
+    const projection = calculateProjection(dot, line);
+
+    const projectionId: string = `p:${hallway.id}->${node.node.id}`;
+
+    const graphNode: GraphNode = {
+        node: {
+            id: projectionId,
+            submapId: node.node.submapId,
+            xCoordinate: projection.x,
+            yCoordinate: projection.y,
+        },
+        neighbours: [node.node]
+    };
+
+    node.neighbours.push(graphNode.node);
+    addHallwayProjection(allHallwayProjections, hallway.id, graphNode);
+}
+
+function addHallwayProjection(hallwayProjections: Map<string, GraphNode[]>, hallwayId: string, graphNode: GraphNode) {
+    if (!hallwayProjections.has(hallwayId)) {
+        hallwayProjections.set(hallwayId, [graphNode]);
     } else {
-        allHallwayProjections.get(hallwayId).push(graphNode);
+        hallwayProjections.get(hallwayId).push(graphNode);
     }
 }
 
 function joinAllHallwayProjections(allHallwayProjections: Map<string, GraphNode[]>, graph: Map<string, GraphNode>) {
     allHallwayProjections.forEach((projections: GraphNode[], hallwayId: string) => {
         for (let i = 0; i < projections.length; i++) {
-            let node1 = projections[i];
+            const node1 = projections[i];
             if (!graph.has(node1.node.id)) {
                 graph.set(node1.node.id, node1);
             }
 
             for (let j = i+1; j < projections.length; j++) {
-                let node2 = projections[j];
+                const node2 = projections[j];
 
                 if (!graph.has(node2.node.id)) {
                     graph.set(node2.node.id, node2);

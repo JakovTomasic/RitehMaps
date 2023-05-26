cc3fe1c4a0ae1564d88a166737efd3a94770b021
Jakov Tomasić <jakov.tomasic@invariant.hr>
2023-05-26 15:09:30 +0200

    Map component pulled from the main branch, /graph page fixed to use it

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -1,52 +1,42 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
 import ZoomableSVG from './ZoomableSVG';
-import { GraphImpl } from '../logic/impl/graph/GraphImpl';
-import { Graph } from '../logic/interfaces/Graph';
-import { MapNode } from '../types/graph/MapNode';
-import { createGraph } from '../logic/impl/graph/GraphFactory';
-import { allGraphData } from '../data/AllGraphData';
+import { NavigationStep } from '../types/NavigationStep';
 
 type Prop = {
     layoutImage: string
-    layoutId?: number
     enableDrawNodes?: boolean
     width: number
     height: number
+    navStep: NavigationStep
 }
 
-const dotRadiusRelative: String = "0.5%";
-const lineStrokeWidthRelative: String = "0.1%";
-const nodeColor: String = "#008a15";
-const connectionColor: String = nodeColor;
+const dotRadiusRelative: String = "0.5%"
+const lineStrokeWidthRelative: String = "0.1%"
+
 
 export default class Map extends Component<Prop>{
 
     mapRef!: SVGSVGElement
 
     private drawMap(){
-
+        
         const svgElement = d3.select(this.mapRef)
         .attr("viewBox", [0, 0, this.props.width, this.props.height])
         .append("image")
         .attr("xlink:href", this.props.layoutImage)
         .attr("width", "100%")
         .attr("height", "100%")
+        
     }
 
-    private drawNodesOnClick(){
 
-        let width = this.props.width;
-        let height = this.props.height;
+    private drawNodesOnClick(){
 
         const svg = d3.select(this.mapRef)
         svg.on("click", function(event) {
 
-            let clickedOn = d3.pointer(event)
-            let decimalPlaces = 2
-            let clickedX = Math.round(clickedOn[0] / width * 100 * 10**decimalPlaces) / 10**decimalPlaces;
-            let clickedY = Math.round(clickedOn[1] / height * 100 * 10**decimalPlaces) / 10**decimalPlaces;
-            console.log(`x: ${clickedX},\ny: ${clickedY},`)
+            console.log(d3.pointer(event))
 
             svg.append("circle")
             .attr("cx", d3.pointer(event)[0])
@@ -59,128 +49,66 @@ export default class Map extends Component<Prop>{
         })
     }
 
-    private connectNodes( {x1, y1, x2, y2} ){
+    private connectNodes(){
+
+        var prevNodeX
+        var prevNodeY
 
         const svg = d3.select(this.mapRef)
 
-        svg.append("circle")
-        .attr("cx", x1)
-        .attr("cy", y1)
-        .attr("r", dotRadiusRelative)
-        .attr("fill", nodeColor)
-        .attr("stroke-opacity", 1)
-        .attr("stroke-width", 150)
-        .classed("animate-pulse", true)
+        this.props.navStep.nodes.forEach((node, index) => {
 
-        // svg.append("circle")
-        // .attr("cx", "0%")
-        // .attr("cy", "0%")
-        // .attr("r", dotRadiusRelative)
-        // .attr("fill","#41C7F7")
-        // .attr("stroke-opacity", 1)
-        // .attr("stroke-width", 150)
-        // .classed("animate-pulse", true)
+            svg.append("circle")
+            .attr("cx", `${node.xCoordinate}%`)
+            .attr("cy", `${node.yCoordinate}%`)
+            .attr("r", dotRadiusRelative)
+            .attr("fill","#41C7F7")
+            .attr("stroke-opacity", 1)
+            .attr("stroke-width", 150)
 
-        // svg.append("circle")
-        // .attr("cx", "0%")
-        // .attr("cy", "100%")
-        // .attr("r", dotRadiusRelative)
-        // .attr("fill","#41C7F7")
-        // .attr("stroke-opacity", 1)
-        // .attr("stroke-width", 150)
-        // .classed("animate-pulse", true)
+            if(index > 0){
+                svg.append("line")
+                .attr("x1", prevNodeX)
+                .attr("y1", prevNodeY)
+                .attr("x2", `${node.xCoordinate}%`)
+                .attr("y2", `${node.yCoordinate}%`)
+                .style("stroke", "#41C7F7")
+                .style("stroke-width", lineStrokeWidthRelative)
+                .attr("stroke-linecap", "round")
+                .attr("stroke-opacity", 0.6)
+            }
 
-        // svg.append("circle")
-        // .attr("cx", "100%")
-        // .attr("cy", "0%")
-        // .attr("r", dotRadiusRelative)
-        // .attr("fill","#41C7F7")
-        // .attr("stroke-opacity", 1)
-        // .attr("stroke-width", 150)
-        // .classed("animate-pulse", true)
-
-        // svg.append("circle")
-        // .attr("cx", "50%")
-        // .attr("cy", "50%")
-        // .attr("r", dotRadiusRelative)
-        // .attr("fill","#41C7F7")
-        // .attr("stroke-opacity", 1)
-        // .attr("stroke-width", 150)
-        // .classed("animate-pulse", true)
-
-        // svg.append("circle")
-        // .attr("cx", "100%")
-        // .attr("cy", "100%")
-        // .attr("r", dotRadiusRelative)
-        // .attr("fill","#41C7F7")
-        // .attr("stroke-opacity", 1)
-        // .attr("stroke-width", 150)
-        // .classed("animate-pulse", true)
-
-        svg.append("circle")
-        .attr("cx", x2)
-        .attr("cy", y2)
-        .attr("r", dotRadiusRelative)
-        .attr("fill", nodeColor)
-        .attr("stroke-opacity", 1)
-        .attr("stroke-width", 150)
-        .classed("animate-pulse", true)
-
-        svg.append("line")
-        .attr("x1", x1)
-        .attr("y1", y1)
-        .attr("x2", x2)
-        .attr("y2", y2)
-        .style("stroke", connectionColor)
-        .style("stroke-width", lineStrokeWidthRelative)
-        .attr("stroke-linecap", "round")
-        .attr("stroke-opacity", 0.6)
-
+            prevNodeX = `${node.xCoordinate}%`
+            prevNodeY = `${node.yCoordinate}%`
+        })
     }
 
 
     componentDidMount() {
 
         this.drawMap()
-        if (typeof this.props.enableDrawNodes !== 'undefined')
+        if (this.props.enableDrawNodes == true)
             this.drawNodesOnClick()
-        
-        let startNode = "main_entrance";
-        let submapId = this.props.layoutId;
-        let constructedGraph = createGraph(allGraphData);
-        let graph = new GraphImpl(constructedGraph);
-        this.recursiveWholeGraph(startNode, submapId, graph, new Set())
+        this.connectNodes()
     }
 
-    // TODO: this is just for testing. Remove this or extract it as a testing tool.
-    recursiveWholeGraph(currentNodeId: string, submapId: number, graph: Graph, visited: Set<string>) {
-        let currentNode: MapNode = graph.getNode(currentNodeId);
-        graph.getNeighbours(currentNodeId).forEach(element => {
-            let edge = `${currentNodeId}--->${element.neighbour.id}`;
-            if (!visited.has(edge)) {
-                visited.add(edge);
+    componentDidUpdate(prevProps: Readonly<Prop>) {
 
-                let neighbourNode = graph.getNode(element.neighbour.id)
-
-                if (element.neighbour.submapId == submapId && currentNode.submapId == submapId) {
-                    this.connectNodes({
-                        x1: `${currentNode.xCoordinate}%`,
-                        y1: `${currentNode.yCoordinate}%`,
-                        x2: `${neighbourNode.xCoordinate}%`,
-                        y2: `${neighbourNode.yCoordinate}%`,
-                    });
-                }
-
-                this.recursiveWholeGraph(element.neighbour.id, submapId, graph, visited);
-            }
-        });
+        if(prevProps.layoutImage != this.props.layoutImage){
+            d3.selectAll("image").remove().exit()
+            this.drawMap()
+        }
+        
+        d3.selectAll("circle").remove().exit()
+        d3.selectAll("line").remove().exit()
+        this.connectNodes()
     }
 
     render() {
     
         return(
             <div>
-                <div className="w-full h-full border" style={{aspectRatio: this.props.width / this.props.height}}>
+                <div className="w-full h-full border max-w-3xl mx-auto" style={{aspectRatio: this.props.width / this.props.height}}>
                     <ZoomableSVG>
                         <svg ref={(mapRef: SVGSVGElement) => this.mapRef = mapRef}>
                             
--- client/src/pages/graph.tsx
+++ client/src/pages/graph.tsx
@@ -1,24 +1,67 @@
 import Map from "../components/Map";
+import { allGraphData } from "../data/AllGraphData";
+import { createGraph } from "../logic/impl/graph/GraphFactory";
+import { GraphImpl } from "../logic/impl/graph/GraphImpl";
+import { Graph } from "../logic/interfaces/Graph";
+import { MapNode } from "../types/graph/MapNode";
+import { NavigationNode } from '../types/NavigationNode';
+import { NavigationStep } from "../types/NavigationStep";
 
-export default function Graph(){
+export default function GraphPage(){
     return(
         <div className="relative w-fill mx-auto my-0">
             {/* TODO: read aspect ratio for the current image from some json file */}
-            <Map layoutImage='/submaps/main_floor_0.svg' layoutId={1} enableDrawNodes width={2809.2} height={847.27997}/>
+            <Map layoutImage='/submaps/main_floor_0.svg' enableDrawNodes width={2809.2} height={847.27997} navStep={navigationStepWithAllNodesFromTheSubmap(1)}/>
             <Separator />
-            <Map layoutImage='/submaps/main_floor_1.svg' layoutId={2} enableDrawNodes width={2851.4399} height={875.59998}/>
+            <Map layoutImage='/submaps/main_floor_1.svg' enableDrawNodes width={2851.4399} height={875.59998} navStep={navigationStepWithAllNodesFromTheSubmap(2)}/>
             <Separator />
-            <Map layoutImage='/submaps/main_floor_2.svg' layoutId={3} enableDrawNodes width={2742.04} height={1092.48}/>
+            <Map layoutImage='/submaps/main_floor_2.svg' enableDrawNodes width={2742.04} height={1092.48} navStep={navigationStepWithAllNodesFromTheSubmap(3)}/>
             <Separator />
-            <Map layoutImage='/submaps/main_floor_3.svg' layoutId={4} enableDrawNodes width={2952.5999} height={1335.9199}/>
+            <Map layoutImage='/submaps/main_floor_3.svg' enableDrawNodes width={2952.5999} height={1335.9199} navStep={navigationStepWithAllNodesFromTheSubmap(4)}/>
             <Separator />
-            <Map layoutImage='/submaps/lab_floor_0.svg' layoutId={101} enableDrawNodes width={2760.1599} height={1083.36}/>
+            <Map layoutImage='/submaps/lab_floor_0.svg' enableDrawNodes width={2760.1599} height={1083.36} navStep={navigationStepWithAllNodesFromTheSubmap(101)}/>
             <Separator />
-            <Map layoutImage='/submaps/lab_floor_1.svg' layoutId={102} enableDrawNodes width={2721.6802} height={800.23999}/>
+            <Map layoutImage='/submaps/lab_floor_1.svg' enableDrawNodes width={2721.6802} height={800.23999} navStep={navigationStepWithAllNodesFromTheSubmap(102)}/>
         </div>
     );
 }
 
 function Separator() {
     return <hr className="h-4 my-8 bg-gray-600 border-0"></hr>
+}
+
+function navigationStepWithAllNodesFromTheSubmap(submapId: number): NavigationStep {
+
+    let startNode = "main_entrance";
+    let constructedGraph = createGraph(allGraphData);
+    let graph = new GraphImpl(constructedGraph);
+    let visited = new Set<string>();
+    visited.add(startNode);
+    const resultPath = recursiveWholeGraph(startNode, submapId, graph, visited);
+    return new NavigationStep(resultPath);
+}
+
+function recursiveWholeGraph(currentNodeId: string, submapId: number, graph: Graph, visited: Set<string>): NavigationNode[] {
+    let nodes: NavigationNode[] = [];
+    let currentNode: MapNode = graph.getNode(currentNodeId);
+    const currentNavNode = new NavigationNode(submapId, currentNode.xCoordinate, currentNode.yCoordinate)
+
+    if (currentNode.submapId == submapId) {
+        nodes.push(currentNavNode);
+    }
+
+    graph.getNeighbours(currentNodeId).forEach(element => {
+        if (!visited.has(element.neighbour.id)) {
+            visited.add(element.neighbour.id);
+
+            const resultNodes = recursiveWholeGraph(element.neighbour.id, submapId, graph, visited);
+            resultNodes.forEach(node => nodes.push(node));
+        }
+        
+        if (currentNode.submapId == submapId) {
+            nodes.push(currentNavNode);
+        }
+    });
+
+    return nodes;
 }

24ea0f1e05229646e55b2f850b2d330c5cde5d6e
Jakov Tomasić <jakov.tomasic@invariant.hr>
2023-06-04 22:59:33 +0200

    conformed map cropper so each step fits well inside bounds
    splitNavigationPathIntoSteps function conformed so each step fits into max width/height, conformed MapCropperImpl to dinamically calculate scale factor, added getNodeBounds util.

--- client/src/logic/impl/MapCropperImpl.ts
+++ client/src/logic/impl/MapCropperImpl.ts
@@ -1,32 +1,30 @@
 import { CentroidScale } from "../../types/navigation/CentroidScale";
 import { NavigationStep } from "../../types/navigation/NavigationStep";
 import { MapCropper } from "../interfaces/MapCropper";
+import { getNodeBounds } from "./graph/Utils";
 
 export class MapCropperImpl implements MapCropper{
 
     crop(navigationStep: NavigationStep, width: number, height: number): CentroidScale {
-        
-        var sumX = 0
-        var sumY = 0
-        var stepScale = 3
 
-        var numOfNodes = navigationStep.nodes.length
-        var scaledWidth = width/stepScale
-        var scaledHeight = height/stepScale
+        const {minX, minY, maxX, maxY} = getNodeBounds(navigationStep.nodes);
+        const centroidX = minX + (maxX - minX)/2;
+        const centroidY = minY + (maxY - minY)/2;
 
-        navigationStep.nodes.forEach((node) => {
-            sumX += node.xCoordinate
-            sumY += node.yCoordinate           
-        })
+        const stepScale = this.getStepScale(maxX - minX, maxY - minY);
 
-        var centroidX = sumX/numOfNodes
-        var centroidY = sumY/numOfNodes
+        const scaledWidth = width/stepScale;
+        const scaledHeight = height/stepScale;
 
-        var translateX = -1*centroidX*width/100 + width/stepScale - scaledWidth/2
-        var translateY = -1*centroidY*height/100 + height/stepScale - scaledHeight/2
+        const translateX = -1*centroidX*width/100 + scaledWidth/2;
+        const translateY = -1*centroidY*height/100 + scaledHeight/2;
 
-        return {translateX, translateY, stepScale, scaledWidth, scaledHeight}
-
+        return {translateX, translateY, stepScale, scaledWidth, scaledHeight};
     }
-    
+
+    private getStepScale(width: number, height: number): number {
+        const scaleWithoutMargins = Math.min(100/width, 100/height);
+        const marginfactor = 0.9;
+        return scaleWithoutMargins * marginfactor;
+    }
 }
--- client/src/logic/impl/graph/Utils.ts
+++ client/src/logic/impl/graph/Utils.ts
@@ -1,5 +1,6 @@
 import { Hallway } from "../../../data/Hallways";
 import { MapNode } from "../../../types/graph/MapNode";
+import { NavigationNode } from "../../../types/navigation/NavigationNode";
 import { Dot, Line } from "../../../utils/Geometry";
 
 export function nodeToDot(node: MapNode): Dot {
@@ -11,4 +12,20 @@ export function hallwayToLine(hallway: Hallway): Line {
         dot1: {x: hallway.x1, y: hallway.y1},
         dot2: {x: hallway.x2, y: hallway.y2},
     }
+}
+
+export function getNodeBounds(nodes: NavigationNode[]): {minX: number, minY: number, maxX: number, maxY: number} {
+    let minX = Number.POSITIVE_INFINITY;
+    let minY = Number.POSITIVE_INFINITY;
+    let maxX = Number.NEGATIVE_INFINITY;
+    let maxY = Number.NEGATIVE_INFINITY;
+
+    nodes.forEach((node) => {
+        minX = Math.min(minX, node.xCoordinate);
+        minY = Math.min(minY, node.yCoordinate);
+        maxX = Math.max(maxX, node.xCoordinate);
+        maxY = Math.max(maxY, node.yCoordinate);
+    });
+
+    return {minX, minY, maxX, maxY};
 }

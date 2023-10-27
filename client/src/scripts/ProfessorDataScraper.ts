046cfd5faff75be4aa6ffddc2c33b029d529bc93
unknown <nikolina.rodin@hotmail.com>
2023-10-28 01:55:05 +0200

    updated the ProfessorData

--- client/src/scripts/ProfessorDataScraper.ts
+++ client/src/scripts/ProfessorDataScraper.ts
@@ -2,6 +2,16 @@ import axios from "axios";
 import cheerio from "cheerio";
 import { ProfessorData } from "../data/ProfessorData";
 
+/*
+To run this script, go to https://cors-anywhere.herokuapp.com/corsdemo and request temporary access to
+the server. After that, call the getProfessorData() function from somewhere inside the index.tsx file 
+and run the application (yarn run dev). Once the home page loads on localhost:3000, the new list of 
+professors should be printed in the console in the JSON fromat. Copy that JSON object and replace the
+old professor data in ProfessorData.ts. To format the document, use the Prettier extension 
+(esbenp.prettier-vscode). Right-click somewhere in the ProfessorData.ts file and choose 
+"Format Document With..." -> "Prettier - Code formatter".
+*/
+
 export function getProfessorData() : object { 
     const contactsURL = "https://cors-anywhere.herokuapp.com/http://www.riteh.uniri.hr/kontakti/"; //temporary solution
     const axiosInstance = axios.create();
